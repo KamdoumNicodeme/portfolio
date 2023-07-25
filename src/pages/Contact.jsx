9c68b42408a50aca583d91cd51fffed68454140e
Nicodeme K <tchietcheujean@yahoo.fr>
2023-07-25 21:26:00 +0100

    add required champs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,11 +11,11 @@ const Contact = () => {
                 <div>
                     <form action="https://getform.io/f/f32eaa45-6395-4139-88f0-27a706cf7d1f" method="POST" className="w-full max-w-md mx-auto">
                         <input type="text" name="name" placeholder="Entrer votre nom "
-                               className="w-full p-2 bg-transparent border-2 rounded-md text-white focus:outline-none mb-4"/>
+                               className="w-full p-2 bg-transparent border-2 rounded-md text-white focus:outline-none mb-4" required/>
                         <input type="email" name="email" placeholder="Entrer votre email"
-                               className="w-full p-2 bg-transparent border-2 rounded-md text-white focus:outline-none mb-4"/>
+                               className="w-full p-2 bg-transparent border-2 rounded-md text-white focus:outline-none mb-4" required/>
                         <textarea name="message" cols="30" rows="6"
-                                  className="w-full p-2 bg-transparent border-2 rounded-md text-white focus:outline-none mb-4" placeholder="Entrer votre message"></textarea>
+                                  className="w-full p-2 bg-transparent border-2 rounded-md text-white focus:outline-none mb-4" placeholder="Entrer votre message" required></textarea>
                         <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Envoyer</button>
                     </form>
                 </div>
